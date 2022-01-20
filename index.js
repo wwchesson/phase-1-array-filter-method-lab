5d340a44b450b7251e124ed7246f2a99768e1e38
Will <willchesson@gmail.com>
2022-01-20 18:18:52 -0500

    commit tree 74b6829b349138c4cc0262c1e596cfebd1894f89

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, bingo) {
+    return array.filter( 
+        match => match.toLowerCase() === bingo.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, beginning) {
+    return array.filter(
+        (match) => match.toLowerCase().indexOf(beginning.toLowerCase()) === 0
+    );
+}
+
+function matchName(array, name) {
+    return array.filter(
+        match => match.name === name
+    );
+}
+    
